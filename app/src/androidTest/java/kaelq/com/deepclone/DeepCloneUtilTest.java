083efaea9ebcb086271fd9c61b1af84419a63e34
zhoufeng <zhoufeng01@xiaoshouyi.com>
2018-06-13 15:02:21 +0800

    first

---
+++ app/src/androidTest/java/kaelq/com/deepclone/DeepCloneUtilTest.java
@@ -0,0 +1,116 @@
+package kaelq.com.deepclone;
+
+import com.google.gson.JsonObject;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class DeepCloneUtilTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void deepClone() throws Exception {
+        One o = new One();
+        o.setNumber(2);
+
+        assertTrue(DeepCloneUtil.deepClone(o) != o);
+        assertTrue(((One) DeepCloneUtil.deepClone(o)).equals(o));
+
+        OneSerializable os = new OneSerializable();
+        os.setNumber(2);
+
+        assertTrue(DeepCloneUtil.deepClone(os) != os);
+        assertTrue(((OneSerializable) DeepCloneUtil.deepClone(os)).equals(os));
+
+        OneParcelable op = new OneParcelable();
+        op.setNumber(2);
+
+        assertTrue(DeepCloneUtil.deepClone(op) != op);
+        assertTrue(((OneParcelable) DeepCloneUtil.deepClone(op)).equals(op));
+    }
+
+    @Test
+    public void deepClone1() throws Exception {
+        List<String> list = new ArrayList<>();
+        list.add("test");
+        assertTrue(DeepCloneUtil.deepClone(list, String.class) != list);
+        assertTrue(DeepCloneUtil.deepClone(list, String.class).equals(list));
+
+
+        List<Integer> listInt = new ArrayList<>();
+        listInt.add(1);
+        assertTrue(DeepCloneUtil.deepClone(listInt, Integer.class) != listInt);
+//        assertTrue(DeepCloneUtil.deepClone(listInt).equals(listInt));
+
+        List<Double> listDou = new ArrayList<>();
+        listDou.add(1.0);
+        assertTrue(DeepCloneUtil.deepClone(listDou, Double.class) != listDou);
+        assertTrue(DeepCloneUtil.deepClone(listDou).equals(listDou));
+
+        List<One> listOne = new ArrayList<>();
+        One one = new One();
+        one.setNumber(1);
+        listOne.add(one);
+        assertTrue(DeepCloneUtil.deepClone(listOne, One.class) != listOne);
+        assertTrue(DeepCloneUtil.deepClone(listOne, One.class).equals(listOne));
+    }
+
+    @Test
+    public void deepClone2() throws Exception {
+        Map<String, String> mapStrStr = new HashMap<>();
+        mapStrStr.put("test", "test");
+        assertTrue(DeepCloneUtil.deepClone(mapStrStr) != mapStrStr);
+        assertTrue(((HashMap) DeepCloneUtil.deepClone(mapStrStr)).equals(mapStrStr));
+
+        Map<Integer, Integer> mapIntInt = new HashMap<>();
+        mapIntInt.put(1, 1);
+        assertTrue(DeepCloneUtil.deepClone(mapIntInt) != mapIntInt);
+//        assertTrue(((HashMap) DeepCloneUtil.deepClone(mapIntInt)).equals(mapIntInt));
+
+        Map<Double, Double> mapDouDou = new HashMap<>();
+        mapDouDou.put(1.0, 1.0);
+        assertTrue(DeepCloneUtil.deepClone(mapDouDou) != mapDouDou);
+//        assertTrue(((HashMap) DeepCloneUtil.deepClone(mapDouDou)).equals(mapDouDou));
+
+    }
+
+    @Test
+    public void deepCloneSerializable() {
+    }
+
+    @Test
+    public void deepCloneParcelable() {
+    }
+
+    @Test
+    public void deepCloneObject() throws Exception {
+        One o = new One();
+        o.setNumber(2);
+        if (DeepCloneUtil.deepCloneObject(o) == o) {
+            throw new Exception("");
+        }
+        assertTrue(((One) DeepCloneUtil.deepCloneObject(o)).equals(o));
+    }
+
+    @Test
+    public void deepCloneArrayList() {
+    }
+
+    @Test
+    public void jsonToArrayList() {
+    }
+
+    @Test
+    public void deepCloneMap() {
+    }
+}
---
+++ app/src/main/java/kaelq/com/deepclone/DeepCloneUtil.java
@@ -0,0 +1,122 @@
+package kaelq.com.deepclone;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*TODO
+ * 1. Add time compare.
+ * 2. Add testcase. Done
+ * 3. Gson int,long,float 2 double
+ * 4. Gson List<List<T>> problem
+ * Support:
+ * 1. Object Serializable  Parcelable
+ * 2. List<String>  List<Object>  List<Double>
+ * 3. Map<String,String>
+ * */
+public class DeepCloneUtil {
+    /*
+     * Object deepClone method
+     * Serializable & Parcelable & Json
+     * */
+    public static Object deepClone(Object o) {
+        if (Serializable.class.isAssignableFrom(o.getClass())) {
+            try {
+                return deepCloneSerializable(o);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (Parcelable.class.isAssignableFrom(o.getClass())) {
+            try {
+                return deepCloneParcelable(o);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return deepCloneObject(o);
+    }
+
+    /*
+     * ArrayList deepClone method
+     * */
+    public static <T> List<T> deepClone(List<T> o, Class<T> clazz) {
+        return deepCloneArrayList(o, clazz);
+    }
+
+    /*
+     * Map deepClone method
+     * */
+    public static <K, V> Map<K, V> deepClone(Map<K, V> map) {
+        return deepCloneMap(map);
+    }
+
+
+    //Serializable
+    private static Object deepCloneSerializable(Object o) throws IOException, ClassNotFoundException {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("object"));
+        oos.writeObject(o);
+        oos.close();
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("object"));
+        Object result = ois.readObject();
+        ois.close();
+        return result;
+    }
+
+
+    //Parcelable
+    private static Object deepCloneParcelable(Object o) throws Exception {
+        Parcel parcel = null;
+        try {
+            parcel = Parcel.obtain();
+            parcel.writeParcelable((Parcelable) o, 0);
+            parcel.setDataPosition(0);
+            return parcel.readParcelable(o.getClass().getClassLoader());
+        } finally {
+            parcel.recycle();
+        }
+    }
+
+    //Json object
+    private static Object deepCloneObject(Object o) {
+        Gson gson = new Gson();
+        String object = gson.toJson(o);
+        return gson.fromJson(object, o.getClass());
+    }
+
+    //ArrayList
+    public static <T> List<T> deepCloneArrayList(List<T> list, Class<T> clazz) {
+        Gson gson = new Gson();
+        String listStr = gson.toJson(list);
+        Type type = new ParameterizedTypeImpl(List.class, new Class[]{clazz});
+        return gson.fromJson(listStr, type);
+    }
+
+    //Map
+    public static <K, V> Map<K, V> deepCloneMap(Map<K, V> map) {
+        String json = new Gson().toJson(map);
+        return new Gson().fromJson(json, new TypeToken<Map<K, V>>() {
+        }.getType());
+    }
+
+
+}
